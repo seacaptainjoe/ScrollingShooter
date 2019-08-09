969e9f68365a19fcdd0179eff5bead85054faffd
seacaptainjoe <joe.held@gmail.com>
2019-08-09 00:16:00 -0400

    Created / updated: -GameEngine.java -GameStarter.java -GameState.java -HUD.java -Renderer.java -SoundEngine.java

---
+++ app/src/main/java/net/joeheld/scrollingshooter/GameStarter.java
@@ -0,0 +1,8 @@
+package net.joeheld.scrollingshooter;
+
+interface GameStarter {
+
+    // This allows the State class to
+    // spawn and despawn objects via the game engine
+    public void deSpawnReSpawn();
+}
